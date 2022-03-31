eae7548e18bb3d9209bf26f2ded39a0d8884b13d
bharthblaster <bharthblaster@gamail.com>
2022-03-31 19:15:05 +0530

    email validator

--- lib/Screens/Register.dart
+++ lib/Screens/Register.dart
@@ -14,13 +14,13 @@ class Register extends StatefulWidget {
 
 class _RegisterState extends State<Register> {
   final _formkeyregister = GlobalKey<FormState>();
-  final _Scafoldkey=GlobalKey<ScaffoldState>();
+  final _Scafoldkey = GlobalKey<ScaffoldState>();
 
   final TextEditingController emailsignup = TextEditingController();
   final TextEditingController passwordsignup = TextEditingController();
   final TextEditingController confirmpasswordsignup = TextEditingController();
   final TextEditingController signupname = TextEditingController();
-  bool _isloading=false;
+  bool _isloading = false;
   bool _obscureText = true;
 
   late String _password;
@@ -32,6 +32,7 @@ class _RegisterState extends State<Register> {
       _obscureText = !_obscureText;
     });
   }
+
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
@@ -46,6 +47,12 @@ class _RegisterState extends State<Register> {
       },
       textInputAction: TextInputAction.next,
       decoration: const InputDecoration(
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
         ),
@@ -54,8 +61,10 @@ class _RegisterState extends State<Register> {
         ),
         hintText: 'Enter your Email',
       ),
+      validator: (val) =>
+          val!.isEmpty || !val.contains("@") ? "enter a valid eamil" : null,
     );
-final emailName=TextFormField(
+    final emailName = TextFormField(
       controller: signupname,
       onSaved: (value) {
         signupname.text = value!;
@@ -65,16 +74,28 @@ final emailName=TextFormField(
         focusedBorder: OutlineInputBorder(
           borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
         ),
+        errorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
         enabledBorder: OutlineInputBorder(
           borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
         ),
         hintText: 'Enter your Name',
       ),
     );
-  final passwordfield = TextFormField(
+    final passwordfield = TextFormField(
       controller: passwordsignup,
       decoration: InputDecoration(
         focusedBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
           borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
         ),
         enabledBorder: const OutlineInputBorder(
@@ -88,11 +109,10 @@ final emailName=TextFormField(
       ),
       validator: (val) => val!.length < 6 ? 'Password too short.' : null,
       onSaved: (val) {
-        _passwordlength=val!;
+        _passwordlength = val!;
         _password = val;
         passwordsignup.text = val;
       },
-      
       textInputAction: TextInputAction.done,
       obscureText: _obscureText,
     );
@@ -102,11 +122,16 @@ final emailName=TextFormField(
         focusedBorder: const OutlineInputBorder(
           borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
         ),
+        errorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
+        focusedErrorBorder: const OutlineInputBorder(
+          borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
+        ),
         enabledBorder: const OutlineInputBorder(
           borderSide: BorderSide(color: CustomColor.cosmic_red, width: 2.0),
         ),
         hintText: 'Confirm Your Password',
-        
         suffixIcon: FlatButton(
             onPressed: _toggle,
             child:
@@ -114,134 +139,134 @@ final emailName=TextFormField(
       ),
       validator: (val) => val!.length < 6 ? 'Password too short.' : null,
       onSaved: (val) {
-        
-        _passwordlength=val!;
+        _passwordlength = val!;
         _password = val;
         confirmpasswordsignup.text = val;
       },
       textInputAction: TextInputAction.done,
       obscureText: _obscureText,
     );
-    
+
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding:  EdgeInsets.all(height / 97.625),
-            child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
+        child: Padding(
+          padding: EdgeInsets.all(height / 97.625),
+          child: ListView(
+            children: [
+              Align(
+                  alignment: Alignment.topLeft,
+                  child: SvgPicture.asset("assets/login_back.svg")),
+              SizedBox(
+                height: twentyheight,
+              ),
+              Text("Lets's Sign You In",
+                  style: GoogleFonts.inter(
+                      color: Colors.black,
+                      fontSize: height35,
+                      fontWeight: FontWeight.bold)),
+              Text("Welcome Back You",
+                  style: GoogleFonts.iceland(
+                    color: Colors.black,
+                    fontSize: 25,
+                  )),
+              Text(
+                "You Were Missed!",
+                style: GoogleFonts.iceland(
+                  color: Colors.black,
+                  fontSize: 25,
+                ),
+              ),
+              SizedBox(
+                height: height / 45,
+              ),
+              Padding(
+                padding: EdgeInsets.fromLTRB(heigth25, 0, heigth25, 0),
+                child: Form(
+                  key: _formkeyregister,
+                  child: Column(
+                    children: [
+                      emailName,
+                      SizedBox(
+                        height: height / 45,
+                      ),
+                      emailfield,
+                      SizedBox(
+                        height: height / 45,
+                      ),
+                      passwordfield,
+                      SizedBox(
+                        height: height / 45,
+                      ),
+                      confirmPasswordfield,
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Align(
+                          alignment: Alignment.topRight,
+                          child: Text(
+                            "Forgot Password?",
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: height / 15.4,
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.center,
+                child: ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      _isloading = true;
+                    });
+                    if (_formkeyregister.currentState!.validate()) {
+                      final snackbar = SnackBar(content: const Text("data"));
+                      _Scafoldkey.currentState!.showSnackBar(snackbar);
+                    }
+                    setState(() {
+                      _isloading = false;
+                    });
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(height / 78.1),
+                    child: Text(
+                      _isloading ? "Loading" : "Signup",
+                      style: TextStyle(fontSize: 25),
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    side: BorderSide(
+                      width: 2.0,
+                      color: CustomColor.cosmic_red,
+                    ),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(14)),
+                    primary: CustomColor.cosmic_red,
+                  ),
+                ),
+              ),
+              Expanded(
+                  child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: [
-                    SvgPicture.asset("assets/login_back.svg"),
-                    SizedBox(
-                      height: twentyheight,
+                    Text(
+                      "Don't have an account? ",
+                      style: GoogleFonts.tinos(fontSize: 20),
                     ),
-                    Text("Lets's Sign You In",
-                        style: GoogleFonts.inter(
-                            color: Colors.black,
-                            fontSize: height35,
-                            fontWeight: FontWeight.bold)),
-                    Text("Welcome Back You",
-                        style: GoogleFonts.iceland(
-                          color: Colors.black,
-                          fontSize: 25,
-                        )),
-                    Text("You Were Missed!",
-                        style: GoogleFonts.iceland(
-                          color: Colors.black,
-                          fontSize: 25,
-                        ),),
-                    SizedBox(
-                      height: height / 45,
+                    Text(
+                      "Create now",
+                      style: GoogleFonts.tinos(
+                          color: CustomColor.cosmic_red, fontSize: 20),
                     ),
-                    Padding(
-                      padding: EdgeInsets.fromLTRB(heigth25, 0, heigth25, 0),
-                      child: Form(
-                        key: _formkeyregister,
-                        child: Column(
-                          children: [
-                            emailName,
-                            SizedBox(
-                              height: height / 45,
-                            ),
-                            emailfield,
-                            SizedBox(
-                              height: height / 45,
-                            ),
-                            passwordfield,
-                             SizedBox(
-                              height: height / 45,
-                            ),
-                            confirmPasswordfield,
-                            GestureDetector(
-                              onTap: () {},
-                              child: const Align(
-                                alignment: Alignment.topRight,
-                                child: Text(
-                                  "Forgot Password?",
-                                  style: TextStyle(color: Colors.grey),
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: height / 15.4,
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: ElevatedButton(
-                        onPressed: () {
-                        setState(() {
-                          _isloading =true;
-                        });
-                        if(_formkeyregister.currentState!.validate()){
-            
-                          final snackbar=SnackBar(content: const Text("data"));
-                          _Scafoldkey.currentState!.showSnackBar(snackbar);
-                        }
-                        
-                        },
-                        child: Padding(
-                          padding: EdgeInsets.all(height / 78.1),
-                          child:  Text(
-                            _isloading?"Loading":
-                            "Signup",
-                            style: TextStyle(fontSize: 25),
-                          ),
-                        ),
-                        style: ElevatedButton.styleFrom(
-                          side:  BorderSide(
-                            width: 2.0,
-                            color: CustomColor.cosmic_red,
-                          ),
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(14)),
-                          primary: CustomColor.cosmic_red,
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                        child: Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            "Don't have an account? ",
-                            style: GoogleFonts.tinos(fontSize: 20),
-                          ),
-                          Text(
-                            "Create now",
-                            style: GoogleFonts.tinos(
-                                color: CustomColor.cosmic_red, fontSize: 20),
-                          ),
-                        ],
-                      ),
-                    ))
                   ],
                 ),
+              ))
+            ],
           ),
         ),
       ),
